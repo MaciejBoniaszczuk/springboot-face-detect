14732ddc8076efd244a66cc1bab9d329999b1cf1
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-10-06 14:20:54 +0200

    feat: ImageUrl model added

---
+++ src/main/java/pl/boniaszczuk/springbootfacedetect/FaceApiClient.java
@@ -0,0 +1,12 @@
+package pl.boniaszczuk.springbootfacedetect;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class FaceApiClient {
+
+    
+}
